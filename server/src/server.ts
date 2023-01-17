a842c4e51cab8e61261599b7a737e7898254c6de
Bruno Mariani <bruno0907@gmail.com>
2023-01-16 22:35:13 -0300

    chore: added prisma cors and habits model and migration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,39 @@
-console.log({ hello: 'hello world' })
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { prisma } from "./lib/prisma";
+
+const port = 3333
+
+const app = Fastify()
+
+app.register(cors)
+
+app.get('/', async () => {  
+  return 'Hello Fastify!'
+})
+
+app.get('/habits', async (req, res) => {
+  const habbits = await prisma.habit.findFirst()
+
+  return res.send(habbits)
+})
+
+app.post('/habits', async (req, res) => {
+  const { title } = req.body
+
+  await prisma.habit.create({
+    data: {
+      title
+    }
+  })
+
+  return res.status(201).send()
+})
+
+app.listen({ port }, (err, address) => {
+  if(err) {
+    app.log.error(err)
+  }
+  console.log(`Server is now listening on ${address}`)
+})
+
