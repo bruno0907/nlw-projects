a4839947f3c2c9caa9460c2f3ef625a55b809a50
Bruno Mariani <bruno0907@gmail.com>
2023-01-17 23:20:21 -0300

    chore: prisma relations fix. added prisma seed file

--- nlw-start/server/prisma/migrations/20230118021725_create_tables/migration.sql
+++ nlw-start/server/prisma/migrations/20230118021725_create_tables/migration.sql
@@ -1,14 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `habbits` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "habbits";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "habits" (
     "id" TEXT NOT NULL PRIMARY KEY,
@@ -20,7 +9,8 @@ CREATE TABLE "habits" (
 CREATE TABLE "habit_week_days" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "habit_id" TEXT NOT NULL,
-    "week_day" INTEGER NOT NULL
+    "week_day" INTEGER NOT NULL,
+    CONSTRAINT "habit_week_days_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -32,15 +22,17 @@ CREATE TABLE "days" (
 -- CreateTable
 CREATE TABLE "day_habits" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
     "habit_id" TEXT NOT NULL,
-    "day_id" TEXT NOT NULL
+    CONSTRAINT "day_habits_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "days" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_habits_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
+CREATE INDEX "days_date_idx" ON "days"("date");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "day_habits_day_id_habit_id_key" ON "day_habits"("day_id", "habit_id");
