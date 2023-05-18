788e48f426ac0415d8662a9409ae5774a1154fd1
Bruno Mariani <bruno0907@gmail.com>
2023-05-17 22:20:11 -0300

    feat: added auth to the web

--- nlw-spacetime/web/src/app/page.tsx
+++ nlw-spacetime/web/src/app/page.tsx
@@ -1,16 +1,20 @@
+import { cookies } from 'next/headers'
 import Copyright from "../components/Copyright";
 import EmptyMemories from "../components/EmptyMemories";
 import Hero from "../components/Hero";
 import SignIn from "../components/SignIn";
+import Profile from '@/components/Profile';
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
    <main className="grid grid-cols-2 min-h-screen">    
     <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">      
       <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full"/>      
       <div className="absolute bottom-0 right-2 top-2 w-2 bg-stripes"/>
 
-      <SignIn />      
+      {!isAuthenticated ? <SignIn /> : <Profile />}
       <Hero />
       <Copyright />
     </div>
