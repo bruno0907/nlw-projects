15e01296aea933f960a038bffbb203756aa7da6d
Bruno Mariani <bruno0907@gmail.com>
2023-05-20 16:44:18 -0300

    feat: added layout page and auth routes

--- nlw-spacetime/mobile/app/memories.tsx
+++ nlw-spacetime/mobile/app/memories.tsx
@@ -1,8 +1,8 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
 export default function Memories() {
   return (
-    <View className="flex-1 justify-center items-center bg-gray-900">
+    <View className="flex-1 items-center justify-center bg-gray-900">
       <Text className="text-2xl text-green-500">Memories</Text>
     </View>
   )
