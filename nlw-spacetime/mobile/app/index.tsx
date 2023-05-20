61b4b45b2d376d1c9f2ceb33e2acda20ab6a648f
Bruno Mariani <bruno0907@gmail.com>
2023-05-20 14:31:17 -0300

    feat: mobile integration with backend and app routing

--- nlw-spacetime/mobile/app/index.tsx
+++ nlw-spacetime/mobile/app/index.tsx
@@ -1,21 +1,67 @@
+import { useEffect } from 'react';
 import { ImageBackground, Text, View, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import * as SecureStore from 'expo-secure-store'
 import { styled } from 'nativewind';
+
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+
+import { api } from '../src/lib/api';
+import { useRouter } from 'expo-router';
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/6b11402ba7f969234ebd',
+};
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold
   })
+
+  const [_, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '6b11402ba7f969234ebd',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery
+  );  
+
+  async function handleGithubOAuthCode(code) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;      
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response]);
 
   if(!hasLoadedFonts) return null
 
@@ -37,7 +83,11 @@ export default function App() {
             <Text className="text-center font-body text-base leading-relaxed text-gray-100">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
           </View>
 
-          <TouchableOpacity activeOpacity={0.7} className="rounded-full bg-green-500 px-5 py-2">
+          <TouchableOpacity 
+            activeOpacity={0.7} 
+            className="rounded-full bg-green-500 px-5 py-2" 
+            onPress={() => signInWithGithub()}
+          >
             <Text className="font-alt text-sm uppercase text-black">Cadastrar lembrança</Text>
           </TouchableOpacity>
           
