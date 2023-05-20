02d6feddda9f58b3dc53de2efef6bcfb5b174112
Bruno Mariani <bruno0907@gmail.com>
2023-05-20 17:36:24 -0300

    feat: added new screen

--- nlw-spacetime/mobile/app/memories.tsx
+++ nlw-spacetime/mobile/app/memories.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native'
 
 export default function Memories() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-2xl text-green-500">Memories</Text>
     </View>
   )
