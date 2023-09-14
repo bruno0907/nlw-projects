37cf1738da1dd7b45ba9cfb94915e22ce5df3b38
Bruno Mariani <bruno0907@gmail.com>
2023-09-13 22:46:08 -0300

    chore: abstract header to its component

--- nlw-ia/web/src/App.tsx
+++ nlw-ia/web/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Github, Wand2 } from "lucide-react";
+import { Wand2 } from "lucide-react";
 import { useCompletion } from "ai/react";
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
@@ -10,6 +10,7 @@ import { Label } from "./components/ui/label";
 import { Slider } from "./components/ui/slider";
 import { VideoInputFormat } from "./components/video-input-form";
 import { PromptSelect } from "./components/prompt-select";
+import { Header } from "./components/header";
 
 export function App() {
   const [temperature, setTemperature] = useState(0.5);
@@ -47,19 +48,7 @@ export function App() {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">upload.ai</h1>
-        <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">Desenvolvido com ♥ na NLW-AI da Rocketseat</span>
-
-          <Separator orientation="vertical" className="h-6"/>
-
-          <Button variant="outline">
-            <Github className="w-4 h-4 mr-2" />
-            Github
-          </Button>
-        </div>
-      </header>
+      <Header />
 
       <main className="flex-1 p-6 flex gap-6">
 
